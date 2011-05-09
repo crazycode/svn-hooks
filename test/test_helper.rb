431f015b933d2df8ae086281e4c99c582bb8e62a
crazycode <crazycode@gmail.com>
2011-05-09 18:25:05 +0800

    done for jira

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,4 +2,3 @@ require 'rubygems'
 require 'test/unit'
 require 'svn_hook_tools'
 #require File.dirname(__FILE__) + '/../lib/svn_hook_tools'
-
--- test/test_jira.rb
+++ test/test_jira.rb
@@ -1,35 +1,36 @@
 require 'test_helper'
 
+SVN_HOOKS_CONFIG_PATH = File.dirname(__FILE__) + '/config/svn_hooks.yml'
+
 class TestJira < Test::Unit::TestCase
   def test_simple
     assert_equal 1, 1
   end
 
   def test_get_issue_number
-    assert_nil getIssueNumber("helo,ixist", "SB")
-    assert_nil getIssueNumber("SB:helo,ixist", "SB")
-    assert_nil getIssueNumber("SB-: helo,ixist", "SB")
-    assert_nil getIssueNumber("helo,ixSBist", "SB")
-    assert_nil getIssueNumber("helo,iSB-xist", "SB")
-    isn = getIssueNumber("SB-1: Test", "SB")
+    jira = JiraHook.new
+    assert_nil jira.get_issue_number("helo,ixist", "SB")
+    assert_nil jira.get_issue_number("SB:helo,ixist", "SB")
+    assert_nil jira.get_issue_number("SB-: helo,ixist", "SB")
+    assert_nil jira.get_issue_number("helo,ixSBist", "SB")
+    assert_nil jira.get_issue_number("helo,iSB-xist", "SB")
+    isn = jira.get_issue_number("SB-1: Test", "SB")
     assert_not_nil isn
     assert_equal "SB-1", isn
-    assert_equal "SB-34234", getIssueNumber("hello SB-34234", "SB")
-    assert_equal "SB-34234", getIssueNumber("hello SB-34234abcd", "SB")
-    assert_equal "SB-34234", getIssueNumber("hello SB-34234", "[Ss][Bb]")
-    assert_equal "SB-34234", getIssueNumber("hello sB-34234abcd", "[Ss][Bb]")
-    assert_equal "SB-34234", getIssueNumber("hello Sb-34234", "[Ss][Bb]")
-    assert_equal "SB-34234", getIssueNumber("hello sB-34234abcd", "[Ss][Bb]")
-    assert_equal "SB-34234", getIssueNumber("hello Sb-34234", "[Ss][Bb]")
-    assert_equal "SB-34234", getIssueNumber("hello sb-34234abcd", "[Ss][Bb]")
+    assert_equal "SB-34234", jira.get_issue_number("hello SB-34234", "SB")
+    assert_equal "SB-34234", jira.get_issue_number("hello SB-34234abcd", "SB")
+    assert_equal "SB-34234", jira.get_issue_number("hello SB-34234", "sb")
+    assert_equal "SB-34234", jira.get_issue_number("hello sB-34234abcd", "sb")
+    assert_equal "SB-34234", jira.get_issue_number("hello Sb-34234", "SB")
+    assert_equal "SB-34234", jira.get_issue_number("hello sB-34234abcd", "Sb")
+    assert_equal "SB-34234", jira.get_issue_number("hello Sb-34234", "sb")
+    assert_equal "SB-34234", jira.get_issue_number("hello sb-34234abcd", "sB")
   end
 
-  private
-  def getIssueNumber(message, keyreg)
-    re = Regexp.new("#{keyreg}-[0-9]+")
-    if message =~ re
-      "#{$&}".upcase
-    end
+
+  def test_check_jira
+    # JiraHook.check("tangliqun", "Message-99:hello", 'Message')
   end
+
 end
 
